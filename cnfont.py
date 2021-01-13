4b3263907b4aece1db6e720004ff3758226ea99e
Xiaosea <maysrp@gmail.com>
2021-01-13 22:40:48 +0800

    Update cnfont.py

--- cnfont.py
+++ cnfont.py
@@ -1,19 +1,26 @@
 from font import fonts
-def pixel(display,x,y,fill=1):
-    sx=32*(y//8)+x
-    sy=y%8
-    display.pixel(sx,sy,fill)
+def pixel(x,y,fill,display):
+    # print(x,y)
+    if x<32 and y<32:
+        sx=32*(y//8)+x
+        sy=y%8
+        display.pixel(sx,sy,fill)
 
 def chinese(ch_str, x_axis, y_axis,display):
     offset_ = 0
-    y_axis = y_axis*8  # 中文高度一行占8个  
-    x_axis = (x_axis*16)  # 中文宽度占16个
+    # y_axis = y_axis*8 # 中文高度一行占8个  
+    # x_axis = (x_axis*16)  # 中文宽度占16个    
+    y_axis = y_axis # 中文高度一行占8个  
+    x_axis = x_axis  # 中文宽度占16个
+
     for k in ch_str:
         code = 0x00  # 将中文转成16进制编码
         data_code = k.encode("utf-8")
+        # print(data_code)
         code |= data_code[0] << 16
         code |= data_code[1] << 8
         code |= data_code[2]
+        # print(code)
         byte_data = fonts[code]
         for y in range(0, 16):
             a_ = bin(byte_data[y]).replace('0b', '')
