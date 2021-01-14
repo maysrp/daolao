42c9d5acb85e39f878f9f4a97cb2a7afeb2421e2
Xiaosea <maysrp@gmail.com>
2021-01-14 22:32:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cnfont import chinese,pixel
+from cnfont import chinese,pixel,image,anime1,anime2,anime3,flx,anime0
 import max7219
 from machine import Pin, SPI
 import time
@@ -10,108 +10,24 @@ display = max7219.Matrix8x8(spi, ss, 16)
 l1="大佬让我过去吧"
 l2="祝您身体健康"
 l3="财源广进"
-def flx(lx,r="r"):
-    if r=="l":
-        r="<"
-    else:
-        r=">"
-    for i in range(len(lx)-1):
-        display.fill(0)
-        chinese(lx[i:i+2],0,0,display)
-        display.text(r+r+r+" ",96,0,1)
-        display.text('----',64,0,1)
-        display.show()
-        time.sleep(0.1)
-        if i<len(lx)-2:
-            display.fill(0)
-            chinese(lx[i:i+3],-4,0,display)
-            display.text(" "+r+r+r,96,0,1)
-            display.text('----',64,0,1)
-            display.show()
-            time.sleep(0.1)
-            display.fill(0)
-            chinese(lx[i:i+3],-8,0,display)
-            display.text(r+" "+r+r,96,0,1)
-            display.text('----',64,0,1)
-            display.show()
-            time.sleep(0.1)
-            display.fill(0)
-            chinese(lx[i:i+3],-12,0,display)
-            display.text(r+r+" "+r,96,0,1)
-            display.text('----',64,0,1)
-            display.show()
-            time.sleep(0.1)
 
 
 
 
 
-def xx():
-    display.fill(0)
-    chinese("谢",0,0,display)
-    time.sleep(0.1)
-    display.show()
-    display.fill(0)
-    chinese("谢",16,0,display)
-    time.sleep(0.1)
-    display.show()
-    display.fill(0)
-    chinese("大",0,16,display)
-    time.sleep(0.1)
-    display.show()
-    display.fill(0)
-    chinese("佬",16,16,display)
-    time.sleep(0.1)
-    display.show()
-    display.fill(0)
-    chinese("谢谢",0,0,display)
-    chinese("大佬",0,16,display)
-    display.show()
-    time.sleep(0.2)
-    display.fill(0)
-    display.show()
-    time.sleep(0.1)
-    chinese("谢谢",0,0,display)
-    chinese("大佬",0,16,display)
-    display.show()
-    time.sleep(0.3)
-    display.fill(0)
-    display.show()
-    time.sleep(0.1)
-    chinese("谢谢",0,0,display)
-    chinese("大佬",0,16,display)
-    display.show()
-    time.sleep(1)
-    display.fill(0)
-    display.show()
-    time.sleep(0.5)
-
-def zm():
-    display.fill(0)
-    chinese("大佬",0,0,display)
-    chinese("最美",0,16,display)
-    display.show()
-    time.sleep(0.5)
-    display.fill(0)
-    display.show()
-    time.sleep(0.2)
-    chinese("大佬",0,0,display)
-    chinese("最美",0,16,display)
-    display.show()
-    time.sleep(1)
-    display.fill(0)
-    display.show()
-    time.sleep(0.2)
-    chinese("大佬",0,0,display)
-    chinese("最美",0,16,display)
-    display.show()
-    time.sleep(2)
 
 
 
-flx(l1)
-flx(l2)
-flx(l3)
-time.sleep(2)
-xx()
-zm()
+
+# flx(l1,display)
+# flx(l2)
+# flx(l3)
+# time.sleep(2)
+
+
+anime0("爱一直在",display)
+# anime1("下次一定",display)
+# anime2("一键三连",display)
+# anime3("拒绝白嫖",display)
+
+image("b.json",display)
